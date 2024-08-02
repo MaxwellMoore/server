d6dc0a4b1b1e6617f80641323ab6f06c944f4e16
Maxwell Moore <maxwellmoore.work@gmail.com>
2024-08-02 13:24:25 -0600

    Added createdAt and updatedAt fields to updateProductSchema

--- src/06-schemas/product.schema.js
+++ src/06-schemas/product.schema.js
@@ -43,6 +43,12 @@ const updateProductSchema = Joi.object({
     status: Joi.string().required().messages({
       "any.required": "Status is required",
     }),
+    updatedAt: Joi.date().required().messages({
+      "any.required": "updatedAt is required",
+    }),
+    createdAt: Joi.date().required().messages({
+      "any.required": "createdAt is required",
+    }),
   }),
   query: Joi.object().optional(),
   params: Joi.object({
